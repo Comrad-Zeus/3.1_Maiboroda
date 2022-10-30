9d65335f37438ac388492973a144f5b690f1e936
Comrad-Zeus <113071286+Comrad-Zeus@users.noreply.github.com>
2022-10-30 19:36:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// 3 вар, лёгкий 
+Console.Write("Введите А:");
+double A = double.Parse(Console.ReadLine());
+
+Console.Write("Введите N:");
+double N = double.Parse(Console.ReadLine());
+
+double result = 1;
+for (int i = 0; i < N; i++)
+{
+    result *= N;
+}
+
+Console.WriteLine(result);
